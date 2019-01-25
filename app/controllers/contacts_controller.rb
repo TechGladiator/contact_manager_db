d4a647b9c7b345f9df60000cba923a46cb415e5b
John Stratton <jstrat0407@gmail.com>
2019-01-25 16:46:35 -0500

    set endpoint for showing single contact by id

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,20 @@
 class ContactsController < ApplicationController
+    before_action :set_contact, only: [:show, :update, :destroy]
+
     # GET /contacts
     def index
         @contacts = TblContact.all
         json_response(@contacts)
+    end
+
+    # GET /contacts/:id
+    def show
+        json_response(@contact)
+    end
+
+    private
+
+    def set_contact
+        @contact = TblContact.find(params[:id])
     end
 end
