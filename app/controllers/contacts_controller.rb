8d6f79d704d2748388941f8176a1398ccf7373a8
John Stratton <jstrat0407@gmail.com>
2019-01-25 17:59:42 -0500

    enabled #create to post contact data
    causes validation error

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,12 +7,23 @@ class ContactsController < ApplicationController
         json_response(@contacts)
     end
 
+    # POST /contacts
+    def create
+        @contact = TblContact.create!(contact_params)
+        json_response(@contact, :created)
+    end
+
     # GET /contacts/:id
     def show
         json_response(@contact)
     end
 
     private
+
+    def contact_params
+        # whitelist params
+        params.permit(:fname, :mname, :lname, :title, :tbl_contact_type_id, :tbl_import_id)
+    end
 
     def set_contact
         @contact = TblContact.find(params[:id])
